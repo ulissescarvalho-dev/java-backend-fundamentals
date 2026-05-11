f367897f01abe21964025ecc7979967af6926926
Ulisses <carvalhou995@gmail.com>
2026-05-11 15:30:54 -0300

    feat: implement worker salary calculation with composition

--- 05-enums-and-composition/src/main/java/com/ulisses/composition/entities/Department.java
+++ 05-enums-and-composition/src/main/java/com/ulisses/composition/entities/Department.java
@@ -1,4 +1,19 @@
 package com.ulisses.composition.entities;
 
 public class Department {
+
+    private String name;
+
+    public Department(String departamentName) {
+        this.name = departamentName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
